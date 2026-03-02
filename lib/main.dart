9e59686bc1534dc36d7b166e549a8ec086297d68
Robin-Combrink <robin@skynamo.com>
2026-03-02 16:03:16 +0200

    Add necessary dart files for repro

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+// Minimal Dart file for analyzer to process
+// The analyzer plugin system should attempt to load the plugin
+// specified in analysis_options.yaml before analyzing this file.
+
+void main() {
+  print('Hello, World!');
+}
